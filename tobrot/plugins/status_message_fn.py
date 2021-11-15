79af9c22357fc1d6eaffea4e3ac540a5405bad33
mukundambbs <83640801+mukundambbs@users.noreply.github.com>
2021-11-15 13:28:52 +0530

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -85,14 +85,14 @@ async def status_message_f(
 
                 percentage = int(file.progress_string(0).split('%')[0])
                 prog = "[{0}{1}]".format("".join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),"".join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]))
-                msg += f"<b>════════════════════════════════</b>\n"
+                msg += f"<b>═══════════════@LinkZz_MBBS═════════════════</b>\n"
                 msg += f"\n<b>{downloading_dir_name}</b>"
                 msg += f"\n<b>{prog}</b>"
-                msg += f"\n<b>Speed</b>: {file.download_speed_string()}"
-                msg += f"\n<b>Status</b>: {file.progress_string()} <b>of</b> {file.total_length_string()}"
-                msg += f"\n<b>ETA:</b> {file.eta_string()}"
+                msg += f"\n<b>➜𝗦𝗽𝗲𝗲𝗱</b>: {file.download_speed_string()}"
+                msg += f"\n<b>➜𝗦𝘁𝗮𝘁𝘂𝘀</b>: {file.progress_string()} <b>of</b> {file.total_length_string()}"
+                msg += f"\n<b>➜𝗘𝗧𝗔:</b> {file.eta_string()}"
                 msg += f"\n{msgg}"
-                msg += f"\n<b>To Cancel:</b> <code>/cancel {file.gid}</code>"
+                msg += f"\n<b>➜𝗧𝗼 𝗖𝗮𝗻𝗰𝗲𝗹:</b> <code>/cancel {file.gid}</code>"
                 msg += "\n"
 
         hr, mi, se = up_time(time.time() - BOT_START_TIME)
@@ -104,9 +104,9 @@ async def status_message_f(
         free = humanbytes(free)
 
         ms_g = (
-            f"<b>Bot Uptime</b>: <code>{hr} : {mi} : {se}</code>\n"
+            f"<b>𝗕𝗼𝘁 𝗨𝗽𝘁𝗶𝗺𝗲</b>: <code>{hr} : {mi} : {se}</code>\n"
             f"<b>T:</b> <code>{total}</code> <b>U:</b> <code>{used}</code> <b>F:</b> <code>{free}</code>\n"
-            f"<b>RAM:</b> <code>{ram}%</code> <b>CPU:</b> <code>{cpu}%</code>\n"
+            f"<b>𝗥𝗔𝗠:</b> <code>{ram}%</code> <b>𝗖𝗣𝗨:</b> <code>{cpu}%</code>\n"
         )
         if msg == "":
             msg = "🤷‍♂️ No Active, Queued or Paused TORRENTs"
